d83a81cadf74fceaf9aebe819ba8efe140bb3f31
Mutasem Tamimi <mutasemtamimi@gmail.com>
2024-10-05 14:39:29 +0300

    Add .gitignore file

--- Nasa/settings.py
+++ Nasa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'corsheaders',
     'spacequake',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +79,10 @@ WSGI_APPLICATION = 'Nasa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
 
